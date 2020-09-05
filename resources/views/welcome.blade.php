3880cd23e5f692afb9e7d2493767fc0c5fbf9e0e
denibro <denisdewdew@gmail.com>
2020-09-05 09:22:44 +0700

    bankai

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,8 +88,13 @@
     <div class="carousel-item active">
       <img src="/tampilan/img/slider/slide3.jpg" class="d-block w-100 gambar-slider" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      <div class="jumbotron">
+  <h1 class="display-4">Hello, world!</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
       </div>
     </div>
     <div class="carousel-item">
